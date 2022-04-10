a18b0b2e9dec6edcad7e1c27a65367e4c99958c3
parkjaebeom <rownrdl9@naver.com>
2022-04-10 13:51:12 +0900

    Comment

--- api/index.js
+++ api/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const request = require('request');
+
 
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
-
+/*
 app.get('/api/get', function (req, res) {
  res.json({test:'Hello World!'});
 });
@@ -15,6 +17,7 @@ app.post('/api/post', function (req, res) {
     console.log(req.body);
     res.json({test:'Hello World!'});
 });
+*/
 app.post('/', function (req, res) {
     // console.log(req.body);
      const data = req.body;
@@ -22,7 +25,24 @@ app.post('/', function (req, res) {
      res.send('api: OK');
      const messageId = req.body['events'][0]['message']['id'];
      console.log(messageId);
+     const options = {
+        url : `https://api-data.line.me/v2/bot/message/${req.body.events[0].message.id}/content`,
+        method: 'get',
+        headers: {
+            'Anthorization': 'Bearer ' + accessToken,
+        },
+        encoding: null
+    };
+    request(options, function(error, response, body){
+        const buffer = new Buffer.from(body);
+        
+        console.log(ffffff);
+
+        console.log(buffer);
     });
+
+ });
+
 
 // app.listen(3000, function () {
 // });
